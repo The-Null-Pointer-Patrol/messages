ad91efc0b5dabc810f088b4f61dfbe7d49fe71ef
marcoff181 <filippomarcon66@gmail.com>
2025-01-23 16:40:26 +0100

    removed calculated pdr, added way to know source id for topology

--- src/node_event.rs
+++ src/node_event.rs
@@ -1,23 +1,25 @@
+use crate::Message;
 use wg_2024::network::NodeId;
 use wg_2024::packet::{NodeType, Packet};
-use crate::Message;
 
 #[derive(Debug, Clone)]
 pub enum NodeEvent {
     PacketSent(Packet),
-    PacketReceived(Packet),
     StartingMessageTransmission(Message),
     MessageSentSuccessfully(Message),
     MessageReceived(Message),
     KnownNetworkGraph(EventNetworkGraph),
-    UpdateDroppedPackets {
-        node: NodeId,
-        num_of_dropped_packets: u64,
-    }
+    //UpdateDroppedPackets {
+    //    node: NodeId,
+    //    num_of_dropped_packets: u64,
+    //},
 }
 
 #[derive(Debug, Clone)]
+/// representation of the network topology as discovered by client/server during flooding, `source`
+/// is the id of the client/server that created the topology
 pub struct EventNetworkGraph {
+    pub source: NodeId,
     pub nodes: Vec<EventNetworkNode>,
 }
 
@@ -25,6 +27,6 @@ pub struct EventNetworkGraph {
 pub struct EventNetworkNode {
     pub node_id: NodeId,
     pub node_type: NodeType,
-    pub num_of_dropped_packets: u64,
+    //pub num_of_dropped_packets: u64,
     pub neighbors: Vec<NodeId>,
 }
